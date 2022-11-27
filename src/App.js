64503bad0f0e88849fc01ae324076f125ae557be
michaelyfu <m888fu@gmail.com>
2022-11-27 12:58:30 -0500

    flipCard basis

--- src/App.js
+++ src/App.js
@@ -3,10 +3,35 @@ import { Card } from '@mui/material';
 import { useState } from "react";
 import foodData from "./assets/food-data.json";
 import Recipe from "./components/Recipe";
+import FlipCard from "./components/FlipCard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.scss";
+
+const cards = [
+  {
+    id: "1",
+    variant: "hover",
+    front: "Hover",
+    back: "Back"
+  },
+  {
+    id: "2",
+    variant: "click",
+    front: "Click",
+    back: "Back"
+  },
+  {
+    id: "3",
+    variant: "focus",
+    front: "Focus",
+    back: "Back"
+  }
+];
 
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [flipped, setFlip] = useState(false);  
   const [total, setTotal] = useState(0);
   const [cart, setCart] = useState("");
   const [shoppingList, setIngredients] = useState(""); //convert this to an array later
@@ -17,11 +42,18 @@ function App() {
     setIngredients(shoppingList + ingredients);
   }  
 
+  function flipCard(flipped) {
+    setFlip(true);
+  }
+
   return (
     <div className="App">
       <h1>HungryHippo888's Recipes</h1> {/* TODO: personalize your bakery (if you want) */}
+      <button>Cost</button>
+      <button>Calories</button>
+
       {foodData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-      <Recipe name={item.name} description={item.description} price={item.price} image={item.image} ingredients={item.ingredients} addToCart={addToCart}></Recipe>
+      <Recipe name={item.name} description={item.description} price={item.price} image={item.image} ingredients={item.ingredients} flipped = {item.flipped} flipCard = {flipCard} addToCart={addToCart}></Recipe>
       ))}
 
       <div>
@@ -31,7 +63,17 @@ function App() {
         <h2>Cart Total: ${total}</h2>
         <h3>Ingredients: {shoppingList}</h3>
       </div>
+      <div className="container">
+      <div className="row h-100">
+        <div class="col d-flex flex-column flex-md-row justify-content-around align-items-center">
+          {cards.map((card) => (
+            <FlipCard key={card.id} card={card} />
+          ))}
+        </div>
+      </div>
     </div>
+    </div>
+    
   );
 }
 
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,12 +1,16 @@
 import "./Recipe.css";
 import { useState } from "react";
 
-export default function Recipe({name, price, description, image, ingredients, addToCart}) {
-    return (
-        <div className="cardWrapper">
-        <img className="image" src={image} alt="food" />
+function Recipe({name, price, description, image, ingredients, flipped, addToCart, flipCard}) {
+  return (
+        <div className="cardWrapper" onClick={() => flipCard(flipped)}>
+          <div className={flipped ? "flipped" : ""}>
+          <img className="image front" src={image} alt="food"/>
+          <h1 className="back">hello</h1>
           <button onClick={() => addToCart(name, price, ingredients)} style={{maxWidth: "200px"}}>Add to Cart</button>  
+        </div>
         </div>
     );
   }
   
+export default Recipe;
